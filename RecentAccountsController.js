a38f29a8fe5d5c37aee6787f345577340a5f3d2d
keshaakeshu333 <149097087+keshaakeshu333@users.noreply.github.com>
2023-10-27 10:41:30 +0530

    Update RecentAccountsController.js

--- RecentAccountsController.js
+++ RecentAccountsController.js
@@ -1,3 +1,5 @@
+ //2. Build a basic lightning component that can query a list of 10 most recently created accounts and display it using a lightning app.
+// step.3 Create a JavaScript Controller for the Lightning Component (RecentAccountsController.js):
 ({
     doInit : function(component, event, helper) {
         var action = component.get("c.getRecentAccounts");
